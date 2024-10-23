8faf1f3984189c4830d2336aeba2241ad888aac6
juliogayosso <j_chalo@hotmail.com>
2024-10-23 12:57:03 -0600

    add models,controllers, routers,services of booking  and reviews

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const routerCity = require('./city.router');
 const routerHotel = require('./hotel.router');
 const routerImage = require('./image.router');
 const routerBooking = require('./booking.router');
+const routerReview = require('./review.router');
 const router = express.Router();
 
 
@@ -13,6 +14,7 @@ router.use('/cities',routerCity)
 router.use('/hotels',routerHotel)
 router.use('/images', routerImage)
 router.use('/bookings',routerBooking)
+router.use('/reviews',routerReview)
 
 
 module.exports = router;
